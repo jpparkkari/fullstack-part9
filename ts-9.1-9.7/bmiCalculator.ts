9080cd40d0167e22fa896f8e24b47ba39e5f87f8
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-11-18 10:47:27 +0200

    9.3 cli bmi. fix to 9.1 bmi formula.

--- ts-9.1-9.7/bmiCalculator.ts
+++ ts-9.1-9.7/bmiCalculator.ts
@@ -1,14 +1,39 @@
 type Result = string;
 
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
 const calculateBmi = (height: number, weight: number) : Result => {
   const bmi : number = weight/(height/100*height/100)
 
   if (bmi < 20) 
     return "Low (underweight)";
-  if (bmi >= 20 || bmi <= 25) 
+  if (bmi >= 20 && bmi <= 25) 
     return "Normal (healthy weight)";
   if (bmi > 25) 
     return "High (overweight)";
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight))
+} catch (e) {
+  console.log('Error, something very bad happened, message: ', e.message)
+}
