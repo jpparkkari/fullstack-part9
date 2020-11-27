7741436ed2d327d12063ddd0a9f1f3dc2e713dcf
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-11-27 12:29:45 +0200

    9.14

--- course-app/src/index.tsx
+++ course-app/src/index.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface HeaderProps {
+  name: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+interface PartsValue {
+  name: string,
+  exerciseCount: number
+}
+
+const Header: React.FC<HeaderProps> = (props)  => {
+  return <h1>{props.name}</h1>;
+}
+
+const Content: React.FC<{parts: Array<PartsValue>}> = (props) => {
+  return (
+    <>
+    {props.parts.map((part,i) => <p key={i}>{ part.name } {part.exerciseCount} </p>
+      
+    )}
+    </>
+  );
+}
+const Total: React.FC<{parts: Array<PartsValue>}> = (props) => {
+  return ( 
+  <p>
+  Number of exercises{" "}
+  {props.parts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+  </p> );
+}
+
+const App: React.FC = () => {
+  const courseName = "Half Stack application development";
+  const courseParts: Array<PartsValue> = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
