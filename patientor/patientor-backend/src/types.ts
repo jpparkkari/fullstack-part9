7f23f9707e34b4507544ea3c618cf46542b30fde
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-12-02 16:03:41 +0200

    9.19

--- patientor/patientor-backend/src/types.ts
+++ patientor/patientor-backend/src/types.ts
@@ -14,10 +14,6 @@ export enum Gender {
 
 export type NewPatient = Omit<Patient, 'id'>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
-
 export interface Patient {
   id: string,
   name: string,
@@ -27,5 +23,41 @@ export interface Patient {
   occupation: string,
   entries: Entry[]
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge?: {date: string, criteria: string};
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {startDate: string, endDate: string};
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
